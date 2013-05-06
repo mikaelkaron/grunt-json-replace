8a6fce06c5f629278a31c1c4e26b5c2e40b95f06
Mikael Karon <mikael@karon.se>
2013-05-06 19:55:56 +0800

    Initial commit

---
+++ tasks/json-replace.js
@@ -0,0 +1,67 @@
+/*
+ * grunt-json-replace
+ * https://github.com/mikaelkaron/grunt-json-replace
+ *
+ * Copyright (c) 2013 Mikael Karon
+ * Licensed under the MIT license.
+ */
+
+"use strict";
+
+module.exports = function (grunt) {
+	var UNDEFINED;
+	var TYPEOF_OBJECT = typeof {};
+	var REPLACE = "replace";
+	var SPACE = "space";
+
+	function walk(node, replace) {
+		var result;
+
+		switch (typeof node) {
+			case TYPEOF_OBJECT:
+				result = {};
+
+				Object.keys(node).forEach(function(key) {
+					result[key] = walk(node[key], replace && replace[key]);
+				});
+				break;
+
+			default:
+				result = replace instanceof Function ? replace(node) : replace || node;
+		}
+
+		return result;
+	}
+
+	grunt.task.registerMultiTask("json-replace", "Read, replace and write json files", function (replace, space) {
+		// Define default options
+		var options = {};
+		options[SPACE] = "\t";
+
+		// Load cli options (with defaults)
+		options = this.options(options);
+
+		// Override options
+		space = options[SPACE] = space || options[SPACE];
+		replace = options[REPLACE] = replace || options[REPLACE];
+
+		// Log flags (if verbose)
+		grunt.log.verbose.writeflags(options);
+
+		try {
+			this.files.forEach(function (file) {
+				var src = file.src;
+				var dest = file.dest;
+
+				grunt.file.write(dest, JSON.stringify(walk(grunt.file.readJSON(src), replace), UNDEFINED, space));
+
+				// Output
+				grunt.log.ok("Read " + src + ", wrote " + dest);
+			});
+		}
+		catch (e) {
+			// Fail with error
+			grunt.fail.warn(e);
+		}
+	});
+};
